ecd6d5c9802c0af3377a3381a636ea03c636e628
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-09-07 11:52:20 +0200

    Use seperate whitelists for iCAM and streamrelay (#4378)
    -Restore streamrelayChecker() and option.
    -Add softCSAChecker() for whitelist NDS-CSA.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -852,6 +852,7 @@ def InitUsageConfig():
 		choices=[(0, _("Disabled"))] + [(x, _("%d ms") % x) for x in range(100, 2001, 100)]
 	)
 
+	config.softcsa.useStreamRelayWhitelist = ConfigYesNo(default=True)
 
 	config.ntp = ConfigSubsection()
 
